2575076e13d1047af9073e10557e6766b22dfad0
PPSI <nurramdandoni@gmail.com>
2020-01-23 11:12:03 +0700

    add action Transactionfragment

--- app/src/main/java/com/ppsi/epayment/TransactionFragment.java
+++ app/src/main/java/com/ppsi/epayment/TransactionFragment.java
@@ -4,20 +4,64 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class TransactionFragment extends Fragment {
+
+    private RelativeLayout btnSend;
+    private RelativeLayout btnReceive;
+    private RelativeLayout btnDeposit;
+    private RelativeLayout btnWithdrawal;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction, container, false);
+        View view = inflater.inflate(R.layout.fragment_transaction, container, false);
+
+        btnSend = view.findViewById(R.id.btnSend);
+        btnReceive = view.findViewById(R.id.btnReceive);
+        btnDeposit = view.findViewById(R.id.btnDeposit);
+        btnWithdrawal = view.findViewById(R.id.btnWithdrawal);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Menu Send Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnReceive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Menu Receive Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnDeposit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Menu Deposit Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnWithdrawal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Menu Withdrawal Clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
 }
